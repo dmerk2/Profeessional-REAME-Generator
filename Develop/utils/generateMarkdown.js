bd47813659e328a52e1f782dc6b7eb947c1dd8d1
Daniel Merkin <dan.merkin@gmail.com>
2022-08-18 11:59:16 -0400

    created function to return generateMarkdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,7 +12,41 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `<h1># ${data.title}</h1>
+
+## Description
+${data.description};
+
+## Table of Contents
+-[Description](#description)
+-[Installation](#installation);
+-[Usage](#installation);
+-[License](#installation);
+-[Contributing](#installation);
+-[Tests](#installation);
+-[Questions](#questions)
+
+## Installation
+Installation instructions:
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This application is ${data.license} license.
+
+## Contributing
+${data.contributing} contributed to this project.
+
+## Tests
+${data.tests}
+
+## Questions
+Have any questions? ${data.questions}
+Shoot me an email at ${data.email}
+Check me out on GitHub! [${data.username}](https://github.com/${data.username})
+Generated with [README-generator](https://github/dmerk2/Professional-README-Generator)
 
 `;
 }
