7c01a8ffafeaa717f41dbaff4023e0b33c68d6c9
Daniel Merkin <dan.merkin@gmail.com>
2022-08-21 16:47:05 -0400

    created function to render badge license

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,28 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-const renderLicenseBadge = (license) => {
-
-}
+// const renderLicenseBadge = (license) => {
+//   if (license === "GNU-Affero-General-Public-License-v3.0") {
+//     return src = "https://raster.shields.io/badge/GNU--Affero--General--Public--License--v3.0-green";
+//   } else if (license === "GNU General Public License v2.0") {
+//     return src = "https://raster.shields.io/badge/-GNU%20General%20Public%20License%20v2.0-blue";
+//   } else if (license === "GNU General Public License v3.0") {
+//     return src = "https://raster.shields.io/badge/-GNU%20General%20Public%20License%3.0-orange";
+//   } else {
+//     return '';
+//   }
+// };
+// renderLicenseBadge(license);
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-const renderLicenseLink = (license) => {
-
-}
+const renderLicenseLink = (license) => {};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-const renderLicenseSection = (license) => {
-  // this.license = license;
-}
+// const renderLicenseSection = (license) => {
+//   if (!license) {
+//     return "";
+//   }
+// };
 
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = (data) => {
@@ -39,7 +48,9 @@ ${data.installation}
 ${data.usage}
 
 ## License
-This application is using ${data.license}(https://www.gnu.org/${data.license}).
+This application is using ${data.license}<br>
+![License](https://www.gnu.org/licenses/agpl-3.0.en.html)<br>
+![Badge](https://img.shields.io/badge/{GNU--Affero--General--Public--License--v3.0-green})
 
 ## Contributing
 ${data.contributing} contributed to this project.
@@ -53,6 +64,6 @@ Check me out on GitHub! [${data.username}](https://github.com/${data.username})<
 Shoot me an email at ${data.email}<br>
 Generated with [README-generator](https://github.com/dmerk2/Professional-README-Generator)
 `;
-}
+};
 
 module.exports = generateMarkdown;
